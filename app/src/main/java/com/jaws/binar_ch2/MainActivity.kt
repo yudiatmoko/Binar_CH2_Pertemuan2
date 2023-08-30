81ae2a5e1c01cdf47a458b683a686159f9fe2e21
yudiatmoko <yudiatmokoilham@gmail.com>
2023-08-31 03:00:42 +0700

    Create Binding

--- app/src/main/java/com/jaws/binar_ch2/MainActivity.kt
+++ app/src/main/java/com/jaws/binar_ch2/MainActivity.kt
@@ -2,10 +2,41 @@ package com.jaws.binar_ch2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import com.jaws.binar_ch2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvNameHelloWorld: TextView? = null
+    private var ownerName: String? = null
+
+    // Declare Binding and Initiate Binding
+    private val binding : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        // Change layout name into binding root
+        setContentView(binding.root)
+        setTextHello()
+    }
+
+    private fun setTextHello() {
+
+        ownerName = "Ilham Yudiatmoko"
+
+        // Old Way
+//        tvNameHelloWorld = findViewById(R.id.tv_hello_world)
+//        tvNameHelloWorld?.text = "Hello, I'am ${ownerName}!"
+//
+//        Toast.makeText(this, tvNameHelloWorld?.text, Toast.LENGTH_SHORT).show()
+//        // Context in Android
+
+        // View Binding
+        binding.tvHelloWorld?.text = "Hello, I'am ${ownerName}!"
     }
 }
